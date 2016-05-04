b6508ceccbbab51cd7c888daace451b9febc0ff5
pmeinhardt <mnhrdt@gmail.com>
2016-05-04 11:53:10 +0200

    Set up OS X (aliases and system preferences)

---
+++ osx/aliases.zsh
@@ -0,0 +1,2 @@
+# Rest a little and activate the screen lock, if locking is enabled.
+alias zz="open /System/Library/Frameworks/ScreenSaver.framework/Versions/Current/Resources/ScreenSaverEngine.app"
