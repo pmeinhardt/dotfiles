a3ccd6bc543e3e9fcdd516de0d86445abb77d3fe
pmeinhardt <mnhrdt@gmail.com>
2019-04-13 13:17:17 +0200

    Remove outdated "zz" alias

--- macos/aliases.zsh
+++
@@ -1,2 +0,0 @@
-# Rest a little and activate the screen lock, if locking is enabled.
-alias zz="open /System/Library/Frameworks/ScreenSaver.framework/Versions/Current/Resources/ScreenSaverEngine.app"
