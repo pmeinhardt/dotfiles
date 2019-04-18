3d59693511af389b13bee0e65a38a02575fc962c
pmeinhardt <mnhrdt@gmail.com>
2019-04-19 00:28:51 +0200

    Remove unnecessary source-ing of ZSH completions
    ZSH will autoload functions in `/usr/local/share/zsh/site-functions` by
    default so there is no need to source completions explicitly.
    
    In case Homebrew's completions are stored in a different directory, the
    directory is appended to the end of the fpath.
    
    For reference, see https://git.io/fjOsZ

--- zsh/fpath.zsh
+++ zsh/fpath.zsh
@@ -3,3 +3,10 @@ for topic_folder ($ZSH/*) if [ -d $topic_folder ]
 then
   fpath=($topic_folder $fpath)
 fi
+
+# Add Homebrew’s completions if they are not in the default ZSH fpath,
+# i.e. they are not in /usr/local/share/zsh/site-functions
+if (( $+commands[brew] )) && [ $(brew --prefix) != "/usr/local" ]
+then
+  fpath=($fpath $(brew --prefix)/share/zsh/site-functions)
+fi
