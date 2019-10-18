db5ce8eff3d6c9b96d32c83f057fae3e0ede5111
pmeinhardt <mnhrdt@gmail.com>
2019-10-18 16:25:49 +0200

    Have epmd only listen on 127.0.0.1

--- erlang/env.zsh
+++ erlang/env.zsh
@@ -1,1 +1,7 @@
+# Enable shell history for Erlang.
 export ERL_AFLAGS='-kernel shell_history enabled'
+
+# At startup, epmd checks the environment variable ERL_EPMD_ADDRESS to figure
+# out what IP addresses to listen on. You can set this variable to 127.0.0.1 to
+# make epmd listen on the loopback interface only.
+export ERL_EPMD_ADDRESS=127.0.0.1
