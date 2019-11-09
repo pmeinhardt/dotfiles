cbdab5318dd898b9d5ee75a603a207e0196356b1
pmeinhardt <mnhrdt@gmail.com>
2019-11-09 23:44:31 +0100

    Move ZSH history key bindings

--- zsh/config.zsh
+++ zsh/config.zsh
@@ -27,9 +27,7 @@ setopt INC_APPEND_HISTORY # adds history incrementally
 setopt HIST_IGNORE_ALL_DUPS # don't record dupes in history
 setopt HIST_REDUCE_BLANKS
 
-# don't expand aliases _before_ completion has finished
-#   like: git comm-[tab]
-setopt complete_aliases
+setopt complete_aliases # don't expand aliases _before_ completion has finished, like: git comm-[tab]
 
 bindkey '^[^[[D' backward-word
 bindkey '^[^[[C' forward-word
@@ -37,3 +35,12 @@ bindkey '^[[5D' beginning-of-line
 bindkey '^[[5C' end-of-line
 bindkey '^[[3~' delete-char
 bindkey '^?' backward-delete-char
+
+# Better history
+# Credits to https://coderwall.com/p/jpj_6q/zsh-better-history-searching-with-arrow-keys
+autoload -U up-line-or-beginning-search
+autoload -U down-line-or-beginning-search
+zle -N up-line-or-beginning-search
+zle -N down-line-or-beginning-search
+bindkey "^[[A" up-line-or-beginning-search # Up
+bindkey "^[[B" down-line-or-beginning-search # Down
