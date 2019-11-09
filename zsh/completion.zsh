3be2452052cd12e63fcf23cba2824c719f30882a
pmeinhardt <mnhrdt@gmail.com>
2019-11-10 00:28:56 +0100

    Enable ZSH completion menu

--- zsh/completion.zsh
+++ zsh/completion.zsh
@@ -3,3 +3,6 @@ zstyle ':completion:*' matcher-list 'm:{a-z}={A-Z}'
 
 # pasting with tabs doesn't perform completion
 zstyle ':completion:*' insert-tab pending
+
+# menu if at least 2 items
+zstyle ':completion:*' menu select=2
