972225cc4a26cb949d6b4fcff78ecd663959f8cb
pmeinhardt <mnhrdt@gmail.com>
2019-11-11 08:23:54 +0100

    Install kubectx for K8s context switching

--- kubernetes/aliases.zsh
+++ kubernetes/aliases.zsh
@@ -1,2 +1,2 @@
-# explicitly initialize kubectl completion if needed to keep shell initialization quick
+# Explicitly initialize kubectl completion if needed to keep shell initialization snappy.
 alias kubectl-init='[ $commands[kubectl] ] && source <(kubectl completion zsh)'
