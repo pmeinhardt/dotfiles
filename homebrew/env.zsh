3f5790bde94f25cf9881721baf6cd5a756bb1a68
pmeinhardt <mnhrdt@gmail.com>
2020-05-19 17:30:49 +0200

    Disable `brew bundle` lock-file generation

--- homebrew/env.zsh
+++ homebrew/env.zsh
@@ -1,5 +1,8 @@
+# Disable sending of analytics data.
+export HOMEBREW_NO_ANALYTICS=1
+
 # Disable beer emoji in homebrew output.
 export HOMEBREW_NO_EMOJI=1
 
-# Disable sending of analytics data.
-export HOMEBREW_NO_ANALYTICS=1
+# Disable lock-file generation (these are for debugging).
+export HOMEBREW_BUNDLE_NO_LOCK=1
