26e401f66f136632c52f00ca813fc83043177155
pmeinhardt <mnhrdt@gmail.com>
2021-01-10 12:52:08 +0100

    Improve colorized man page viewing with bat
    Remove man page output from the terminal when closing the man page, as
    is the normal behavior when using the default pager (less).

--- system/man.zsh
+++ system/man.zsh
@@ -1,5 +1,5 @@
 # Use "bat" for colorizing man pages if available
 if (( $+commands[bat] ))
 then
-  export MANPAGER="sh -c 'col -bx | bat -l man -p'"
+  export MANPAGER="sh -c 'col -bx | bat -l man -p --pager=\"less -FR\"'"
 fi
