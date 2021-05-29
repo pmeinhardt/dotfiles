6de5a79fa9c89e32fd9a0bbe831053fbf191f8c6
Paul Meinhardt <mnhrdt@gmail.com>
2021-05-29 10:20:25 +0200

    Update pyenv initialization
    Version 2.0 changed the way pyenv is initialized.

--- python/pyenv.zsh
+++ python/pyenv.zsh
@@ -1,5 +1,13 @@
 # Explicitly initialize pyenv if needed to keep shell initialization snappy.
 
 if (( $+commands[pyenv] )); then
-  alias pyenv-init='eval "$(pyenv init -)"'
+  function pyenv-init {
+    # Add pyenv executable to PATH and enable shims:
+    export PYENV_ROOT="$HOME/.pyenv"
+    export PATH="$PYENV_ROOT/bin:$PATH"
+    eval "$(pyenv init --path)"
+
+    # Load pyenv into the shell:
+    eval "$(pyenv init -)"
+  }
 fi
