26f2e08c7192da9f13aae84636b56ba7dad87d89
Paul Meinhardt <mnhrdt@gmail.com>
2021-08-20 06:42:11 +0200

    Initialize `nvm` on shell start

--- node/aliases.zsh
+++
@@ -1,3 +0,0 @@
-# Explicitly initialize nvm if needed to keep shell initialization snappy.
-# For reference, see: https://github.com/creationix/nvm/issues/782
-alias nvm-init='export NVM_DIR="$HOME/.nvm" && [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" && [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"'
