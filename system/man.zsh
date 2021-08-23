2a39be81ec08f65019c697570412706cceb6fb2a
Paul Meinhardt <mnhrdt@gmail.com>
2021-08-23 09:38:10 +0200

    Use Neovim as a MANPAGER if available

--- system/man.zsh
+++ system/man.zsh
@@ -1,5 +1,7 @@
-# Use "bat" for colorizing man pages if available
-if (( $+commands[bat] ))
-then
+# Use "nvim" for browsing man pages if available, otherwise use "bat" for
+# colorizing man pages if installed.
+if (( $+commands[nvim] )); then
+  export MANPAGER="nvim +Man!"
+elif (( $+commands[bat] )); then
   export MANPAGER="sh -c 'col -bx | bat -l man -p --pager=\"less -FR\"'"
 fi
