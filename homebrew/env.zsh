66e5efa78ffdc5a85a64b4f6370ebc0e4f671245
Paul Meinhardt <mnhrdt@gmail.com>
2021-11-16 09:54:58 +0100

    Homebrew: Forbid redirects to insecure HTTP

--- homebrew/env.zsh
+++ homebrew/env.zsh
@@ -4,5 +4,8 @@ export HOMEBREW_NO_ANALYTICS=1
 # Disable beer emoji in homebrew output.
 export HOMEBREW_NO_EMOJI=1
 
+# Forbid redirects from secure HTTPS to insecure HTTP.
+export HOMEBREW_NO_INSECURE_REDIRECT=1
+
 # Disable lock-file generation (these are for debugging).
 export HOMEBREW_BUNDLE_NO_LOCK=1
