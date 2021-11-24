bc6d955ed94110a2b64f355b1f3bbeb59958b4db
Paul Meinhardt <mnhrdt@gmail.com>
2021-11-24 10:48:34 +0100

    Install default npm packages with nvm
    Always install "neovim" provider package for every new Node.js version.

--- node/install.sh
+++ node/install.sh
@@ -5,3 +5,7 @@ NVM_DIR="$HOME/.nvm"
 if test ! -d "$NVM_DIR"; then
   git clone "https://github.com/nvm-sh/nvm.git" "$NVM_DIR"
 fi
+
+if test ! -s "$NVM_DIR/default-packages"; then
+  ln -s "$(realpath "$(dirname "$0")/default-packages")" "$NVM_DIR/default-packages"
+fi
