0a92bd647512343d623cc47ade9f6499e4cc98aa
pmeinhardt <mnhrdt@gmail.com>
2022-04-11 16:03:33 +0200

    Speed up shell initialization, drop nvm completion
    Profiling with `zprof` showed that the two main time sinks when starting
    a new shell are:
    
      1. The nvm bash completion
      2. Loading and setting up nvm itself
    
    Since I do not use nvm often, I decided to remove its shell completion
    for now. The nvm setup itself can be streamlined by using its `--no-use`
    flag which just means it will no longer automatically load a Node.js
    version based on a `.nvmrc` file. Instead, you now have to "manually"
    call `nvm use` to do so.

--- node/nvm.zsh
+++ node/nvm.zsh
@@ -1,3 +1,7 @@
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
-[ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
+
+# Load nvm (defer version switching until `nvm use` is invoked)
+[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh" --no-use
+
+# Load nvm shell completion
+# [ -s "$NVM_DIR/bash_completion" ] && . "$NVM_DIR/bash_completion"
