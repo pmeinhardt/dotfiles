282b141dc1a4a901c4ffc678f1b679d7649baca3
pmeinhardt <mnhrdt@gmail.com>
2022-10-16 19:45:35 +0200

    Fix Homebrew setup on Intel Macs
    Homebrew installs to different prefixes on Apple Silicon and Intel Macs:
    https://docs.brew.sh/Installation

--- homebrew/path.zsh
+++ homebrew/path.zsh
@@ -1,1 +1,8 @@
-eval "$(/opt/homebrew/bin/brew shellenv)"
+# Search for Homebrew in its preferred locations:
+# https://docs.brew.sh/Installation
+for prefix in "/opt/homebrew" "/usr/local"; do
+  if [ -x "$prefix/bin/brew" ]; then
+    eval "$($prefix/bin/brew shellenv)"
+    break
+  fi
+done
