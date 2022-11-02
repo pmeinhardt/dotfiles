eb16cbc5e10c3d1b87ff7fe01ef45840e7909ea5
pmeinhardt <mnhrdt@gmail.com>
2022-11-02 17:52:23 +0100

    Remove superfluous script comment

--- homebrew/install.sh
+++ homebrew/install.sh
@@ -1,11 +1,5 @@
 #!/bin/sh
-#
-# Homebrew
-#
-# This installs some of the common dependencies needed (or at least desired)
-# using Homebrew.
 
-# Check for Homebrew
 if test ! $(which brew)
 then
   echo "  Installing Homebrew for you."
