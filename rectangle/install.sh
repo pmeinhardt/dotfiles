bd65bbeb7eaf220ccd458082280f935893757d4d
pmeinhardt <code@mnhrdt.io>
2025-01-01 17:47:00 +0100

    Update Rectangle.app configuration setup

--- rectangle/install.sh
+++ rectangle/install.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-# Configure Rectangle.app.
+# Copy configuration for Rectangle.app.
 
 if [ "$(uname -s)" = "Darwin" ]; then
   src=$(realpath "$(dirname "$0")/RectangleConfig.json")
   dst="$HOME/Library/Application Support/Rectangle/RectangleConfig.json"
   mkdir -p "$(dirname "$dst")"
-  ln -sf "$src" "$dst"
+  cp "$src" "$dst"
 fi
