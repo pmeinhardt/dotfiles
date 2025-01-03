036d44faefbba374e04717d87e0115b262ab38f0
pmeinhardt <code@mnhrdt.io>
2025-01-03 11:01:36 +0100

    Simplify prompt even further

--- zsh/prompt.zsh
+++ zsh/prompt.zsh
@@ -29,43 +29,7 @@
 # \e[K    => clears everything after the cursor on the current line
 # \e[2K   => clear everything on the current line
 
-prompt_pure_set_title() {
-	setopt localoptions noshwordsplit
-
-	# Emacs terminal does not support setting the title.
-	(( ${+EMACS} )) && return
-
-	case $TTY in
-		# Don't set title over serial console.
-		/dev/ttyS[0-9]*) return;;
-	esac
-
-	# Show hostname if connected via SSH.
-	local hostname=
-	if [[ -n $prompt_pure_state[username] ]]; then
-		# Expand in-place in case ignore-escape is used.
-		hostname="${(%):-(%m) }"
-	fi
-
-	local -a opts
-	case $1 in
-		expand-prompt) opts=(-P);;
-		ignore-escape) opts=(-r);;
-	esac
-
-	# Set title atomically in one print statement so that it works when XTRACE is enabled.
-	print -n $opts $'\e]0;'${hostname}${2}$'\a'
-}
-
-prompt_pure_preexec() {
-	# Shows the current directory and executed command in the title while a process is active.
-	prompt_pure_set_title 'ignore-escape' "$PWD:t: $2"
-}
-
 prompt_pure_precmd() {
-	# Shows the full path in the title.
-	prompt_pure_set_title 'expand-prompt' '%~'
-
 	# Make sure VIM prompt is reset.
 	prompt_pure_reset_prompt_symbol
 }
@@ -122,7 +86,7 @@ prompt_pure_state_setup() {
 		fi
 
 		local reIPv6='(([0-9a-fA-F]+:)|:){2,}[0-9a-fA-F]+'  # Simplified, only checks partial pattern.
-		local reIPv4='([0-9]{1,3}\.){3}[0-9]+'   # Simplified, allows invalid ranges.
+		local reIPv4='([0-9]{1,3}\.){3}[0-9]+'  # Simplified, allows invalid ranges.
 		# Here we assume two non-consecutive periods represents a
 		# hostname. This matches `foo.bar.baz`, but not `foo.bar`.
 		local reHostname='([.][^. ]+){2}'
@@ -184,12 +148,11 @@ prompt_pure_setup() {
 		prompt:success       white
 		user                 242
 		user:root            default
-        continuation         242
+		continuation         242
 	)
 	prompt_pure_colors=("${(@kv)prompt_pure_colors_default}")
 
 	add-zsh-hook precmd prompt_pure_precmd
-	add-zsh-hook preexec prompt_pure_preexec
 
 	prompt_pure_state_setup
 
@@ -202,16 +165,16 @@ prompt_pure_setup() {
 		add-zle-hook-widget zle-keymap-select prompt_pure_update_vim_prompt_widget
 	fi
 
-    # Initial newline, for spaciousness.
-    PROMPT='${prompt_newline}'
+	# Initial newline, for spaciousness.
+	PROMPT='${prompt_newline}'
 
 	# Username and machine, if applicable.
 	[[ -n $prompt_pure_state[username] ]] && PROMPT+='$prompt_pure_state[username]%F{${prompt_pure_colors[path]}}:%f'
 
-    # Path.
-    PROMPT+='%F{${prompt_pure_colors[path]}}%1~%f '
+	# Path.
+	PROMPT+='%F{${prompt_pure_colors[path]}}%1~%f '
 
-    # Prompt symbol with success/error color.
+	# Prompt symbol with success/error color.
 	PROMPT+='%(?.%F{$prompt_pure_colors[prompt:success]}.%F{$prompt_pure_colors[prompt:error]})${prompt_pure_state[prompt]}%f '
 
 	# Indicate continuation prompt by … and use a darker color for it.
@@ -228,11 +191,11 @@ prompt_pure_setup() {
 	# information.
 	local -A ps4_parts
 	ps4_parts=(
-		depth 	  '%F{yellow}${(l:${(%)prompt_pure_debug_depth[1]}::+:)}%f'
+		depth     '%F{yellow}${(l:${(%)prompt_pure_debug_depth[1]}::+:)}%f'
 		compare   '${${(%)prompt_pure_debug_depth[2]}:#${(%)prompt_pure_debug_depth[3]}}'
 		main      '%F{blue}${${(%)prompt_pure_debug_depth[3]}:t}%f%F{242}:%I%f %F{242}@%f%F{blue}%N%f%F{242}:%i%f'
 		secondary '%F{blue}%N%f%F{242}:%i'
-		prompt 	  '%F{242}>%f '
+		prompt    '%F{242}>%f '
 	)
 	# Combine the parts with conditional logic. First the `:+` operator is
 	# used to replace `compare` either with `main` or an ampty string. Then
