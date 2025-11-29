6287b1e764ee8c6153275c4b3eea0e88be062421
pmeinhardt <code@mnhrdt.io>
2025-11-29 15:59:06 +0100

    Set DO_NOT_TRACK=1 environment variable
    More and more tools seem to be adopting the DO_NOT_TRACK convention,
    also known as "Console Do Not Track" or "DNT".

--- system/env.zsh
+++ system/env.zsh
@@ -1,5 +1,9 @@
+# Set the default editor
 if (( $+commands[nvim] )); then
   export EDITOR='nvim'
 elif (( $+commands[vim] )); then
   export EDITOR='vim'
 fi
+
+# Disable tracking
+export DO_NOT_TRACK=1
